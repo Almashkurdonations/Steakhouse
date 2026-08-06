5549f13c1a428183bd04356785dd5dd5d603d39e
Almashkurdonations <163398684+Almashkurdonations@users.noreply.github.com>
2026-08-06 21:09:15 +0100

    Update Navbar.tsx

---
+++ Src/Components/Navbar.tsx
@@ -0,0 +1,25 @@
+import "./../styles/navbar.css";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+
+      <div className="logo">
+        EMBER
+      </div>
+
+      <ul>
+
+        <li>Menu</li>
+
+        <li>About</li>
+
+        <li>Gallery</li>
+
+        <li>Reserve</li>
+
+      </ul>
+
+    </nav>
+  );
+}
